31e82d32d3c8272d2fd9fc584107ee8b18bba293
chnihy <chnihy@gmail.com>
2022-05-17 12:21:08 -0500

    moved to seperate repo

---
+++ README.md
@@ -0,0 +1,36 @@
+# IMDB API Flask App
+
+A (very) minimal IMDB API App that simply returns a full resolution poster for any show in IMDB's database.  
+
+Built using Flask
+
+# Requirements
+Requires Flask
+```bash
+pip3 install flask
+```	
+
+# How to run
+1. Download app as zip or clone repo
+```
+git clone https://github.com/chnihy/apis.git
+```
+
+2. Change to app directory
+```
+cd apis/imdb_api_app
+```
+
+3. Run app
+```
+python3 main.py
+```
+
+Open your browser and go to
+```
+localhost:5000
+```
+
+# Future Plans
+1. Incorporate more API features: trailer, cast, reviews etc...
+2. Facelift with Bootstrap
---
+++ main.py
@@ -0,0 +1,49 @@
+import requests
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+api_key = "k_7e4awbkm"
+
+def get_by_title(title):
+	url = f'https://imdb-api.com/API/Search/{api_key}/{title}'
+	r = requests.get(url).json()
+	return r
+
+def get_show(title):
+	results = get_by_title(title)
+	
+	results = results['results']
+
+	first_result = results[0]
+
+	print("first result: {}".format(first_result))
+
+	id = first_result['id']
+
+	#print("r: {}".format(r))
+
+	return first_result
+	
+
+@app.route('/')
+@app.route('/index')
+def index_get():
+
+	return render_template('index.html')
+
+
+
+@app.route('/', methods=["POST"])
+def index_post():
+	
+	title = request.form.get('title')
+
+	if title:
+		message = "Poster for {} goes here".format(title)
+		info = get_show(title)
+		image = info['image']
+		return render_template('index.html', message=message, image=image)
+	
+	#return render_template('index.html')
+
+
